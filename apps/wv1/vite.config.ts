a854283a4c3655f0f1eef74c6f783ea9704274fc
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-11-29 11:36:05 +0200

    added two apps

---
+++ apps/wv1/vite.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+  plugins: [
+    tsconfigPaths({
+      root: '../../',
+      projects: ['tsconfig.base.json'],
+    }),
+  ],
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
+});
