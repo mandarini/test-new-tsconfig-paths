3216fea22622ab41511f222f05cd6570acc0e3f3
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-12-01 12:49:37 +0200

    trying out with alpha 5

--- apps/rv1/vite.config.ts
+++ apps/rv1/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths({
-      projects: ['../../tsconfig.base.json'],
+      root: '../../',
     }),
   ],
 
--- libs/ui/four/vite.config.ts
+++ libs/ui/four/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     }),
     react(),
     tsconfigPaths({
-      projects: ['../../../tsconfig.base.json'],
+      root: '../../../',
     }),
   ],
 
